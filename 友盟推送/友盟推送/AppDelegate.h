19a3c272d4225f720df93aba2baad029e9b72f90
macbook <592944119@qq.com>
2016-05-16 17:01:13 +0800

    创建文件

---
+++ 友盟推送/友盟推送/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  友盟推送
+//
+//  Created by 赵宏亚 on 16/5/16.
+//  Copyright © 2016年 赵宏亚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 友盟推送/友盟推送/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  友盟推送
+//
+//  Created by 赵宏亚 on 16/5/16.
+//  Copyright © 2016年 赵宏亚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
